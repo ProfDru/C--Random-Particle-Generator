5f54e8de98d98b071ce8f1da6645ae8c4dc11337
Prof. Dru <dball89@optonline.net>
2020-10-01 14:06:40 -0400

    inverse_lerp now handles values greater or less than its range

--- src/math/base.h
+++ src/math/base.h
@@ -2,6 +2,7 @@
 #include <concepts>
 #include <numeric>
 #include <cassert>
+#include <algorithm>
 
 namespace rpg::math {
 
@@ -11,6 +12,7 @@ concept Numeric = std::convertible_to<T, double>;
 /*! \brief Get the normalized position of val between a and b */
 template <Numeric T>
 inline constexpr auto inverse_lerp(auto a, auto b, T val) {
+  val = std::clamp(val, a, b);
   return ((abs(val) - a) / (b - a));
 }
 
