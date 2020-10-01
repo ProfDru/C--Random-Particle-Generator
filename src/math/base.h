17b65626c2bef23f529a3f4179ea73b3e74ceb4c
Prof. Dru <dball89@optonline.net>
2020-10-01 15:30:10 -0400

    Add wrap function. Cast values in inverse_lerp

--- src/math/base.h
+++ src/math/base.h
@@ -10,10 +10,19 @@ template <typename T>
 concept Numeric = std::convertible_to<T, double>;
 
 /*! \brief Get the normalized position of val between a and b */
-template <Numeric T>
-inline constexpr auto inverse_lerp(auto a, auto b, T val) {
-  val = std::clamp(val, a, b);
-  return ((abs(val) - a) / (b - a));
+inline constexpr auto inverse_lerp(Numeric auto a,
+                                   Numeric auto b,
+                                   Numeric auto val) {
+  auto aa = static_cast<decltype(val)>(a);
+  auto bb = static_cast<decltype(val)>(b);
+
+  val = std::clamp(val, aa, bb);
+  return ((abs(val) - aa) / (bb - aa));
+}
+
+/*! \brief Wrap the number from 0.0 to 1.0 */
+inline constexpr double wrap(Numeric auto& num) {
+  return num - std::floor(num);
 }
 
 }  // namespace rpg::math
