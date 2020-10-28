7092ac60606b52750729df9bcec760e8388a6adc
Prof. Dru <dball89@optonline.net>
2020-10-28 12:28:12 -0400

    Bounce calculations only modify y-component, replay is more accurate
    - Replay now just applies the velocity calculations in reverse, preventing the x/z calculations from being doubled for time since collision
    - Bounce calculations previously modified all components of the velocity vector. This was inaccurate  since it should only impact the velocity in the direction perpendicular to the point of impact.

--- src/physics/rigid_body.h
+++ src/physics/rigid_body.h
@@ -43,25 +43,25 @@ inline void bounce_basic(RigidBody auto& body,
   const double distance_underground = abs(collision_pos - get(body.pos, 1));
   const double time_since_collision = distance_underground / y_vel;
 
+  // Rewind the particle to the time just before the point of collision
+  physics::apply_velocity(body, -time_since_collision);
+
   // Calculate how much energy was lost with the bounce
-  const Numeric auto energy = kinematic_energy(body, mass);
-  const auto energy_loss = calculate_energy_loss(e);
-  const auto energy_after_bounce = energy_loss * energy;
+  const double energy = kinematic_energy(y_vel, mass);
+  const double energy_loss = calculate_energy_loss(e);
+  const double energy_after_bounce = energy - (energy_loss * energy);
 
   // Update the velocity of the body based on the new energy
   const auto velocity_after_bounce =
       velocity_from_kinematic_energy(energy_after_bounce, mass);
-  set_magnitude(body.velocity, velocity_after_bounce);
+  set(body.velocity, 1, velocity_after_bounce);
 
   // Invert the y-direction of the velocity since it has bounced then
-  // move the particle since it's bounce, and set the position to the
-  // point of collision
-  set(body.velocity, 1, -get(body.velocity, 1));
-  set(body.pos, 1, collision_pos);
+  // move the particle since it's bounce
+  // set(body.velocity, 1, -get(body.velocity, 1));
 
-  // replay the motion of the object since the time of impact
-  if (time_since_collision > 0) {
-    physics::apply_velocity(body, time_since_collision);
-  }
-}
+  // replay the motion of the object since the time of impact with the new
+  // velocity
+  physics::apply_velocity(body, time_since_collision);
+}  // namespace rpg::physics
 }  // namespace rpg::physics
